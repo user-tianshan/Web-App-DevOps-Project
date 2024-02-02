d818fbae3af28240d0dd53607a243dc5a347f891
user-tianshan <105794465+user-tianshan@users.noreply.github.com>
2024-02-02 08:03:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 import pyodbc
 import os
-import azure.identity
-import azure.keyvault.secrets
+#import azure.identity
+#import azure.keyvault.secrets
 
 # Initialise Flask App
 app = Flask(__name__)
 
 
 # Managed Identity
-from azure.identity import ManagedIdentityCredential
-from azure.keyvault.secrets import SecretClient
-key_vault_url = "https://aks-rg-key-vault.vault.azure.net/"
-credential = ManagedIdentityCredential()
-secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
+#from azure.identity import ManagedIdentityCredential
+#from azure.keyvault.secrets import SecretClient
+#key_vault_url = "https://aks-rg-key-vault.vault.azure.net/"
+#credential = ManagedIdentityCredential()
+#secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
 #database_name = secret_client.get_secret("database-name")
 #server_name=secret_client.get_secret("server-name")
 #server_password=secret_client.get_secret("server-password")
